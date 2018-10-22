11f13a1af4c6cfebac4209d7f98cfb34bd99f736
Francisco <francisco.teixeira.de.barros@outlook.com>
2018-10-22 15:18:20 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
 start F:\GitHub\KChat\kiko-chat-client-gui\bin\Debug\kiko-chat-client-gui.exe
 start F:\GitHub\KChat\kiko-chat-client-gui\bin\Debug\kiko-chat-client-gui.exe
-start F:\GitHub\KChat\kiko-chat-client-gui\bin\Debug\kiko-chat-client-gui.exe
 start F:\GitHub\KChat\kiko-chat-server-console\bin\Debug\kiko-chat-server-console.exe
